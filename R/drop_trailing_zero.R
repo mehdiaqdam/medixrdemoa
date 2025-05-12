4c38d24061dbf5eedd9426379b8cd676558f4951
Mehdi Aqdam <mehdimoslemiaqdam@gmail.com>
2025-05-11 22:41:04 -0400

    tests completed

--- R/drop_trailing_zero.R
+++ R/drop_trailing_zero.R
@@ -4,6 +4,7 @@
 #' Drops trailing zero(s) with no mathematical value.
 #'
 #' @param x a numeric value or vector.
+#' @param align alignment ('left' or 'right') of returned values.
 #'
 #' @return a character (single or vector depending on \code{x}).
 #'
@@ -18,14 +19,27 @@
 #'
 #' x <- c(0.00200, 0.1, 1110.0250)
 #' drop_trailing_zero(x)
-drop_trailing_zero <- function(x) {
+drop_trailing_zero <- function(x, align) {
 
-  if (!is.numeric(x)) {
-    cli_abort("Input must be a number a numeric vector.")
+  if (is.null(align) || !align %in% c("left", "right")) {
+    cli_abort("{.arg align} must be defined, and as either 'left' or 'right.")
   }
 
-  x_f = format(x, scientific = FALSE)
-  ifelse(str_detect(x_f, "\\."),
-         str_replace(x_f, "[\\.]?[0]+$", ""),
-         x_f)
+  if (!is.numeric(x)) {
+    cli_abort("Input must be a number or a numeric vector.")
+  }
+
+  x_res = format(x, scientific = FALSE, nsmall = 10)
+  x_res <- ifelse(str_detect(x_res, "\\."),
+                  str_replace(x_res, "[\\.]?[0]+$", ""),
+                  x_res)
+  #
+  # if (str_detect(x_res, "\\.")) {
+  #   x_res <- str_replace(x_res, "[\\.]?[0]+$", "")
+  # }
+
+  if (align == "left") {
+    x_res <- str_trim(x_res)
+  }
+x_res
 }
--- tests/testthat/test-drop_trailing_zero.R
+++ tests/testthat/test-drop_trailing_zero.R
@@ -1,20 +1,43 @@
-test_that("input is numeric", {
-  x <- "2"
-  expect_error(drop_trailing_zero(x), "Input must be a number a numeric vector.")
+test_that("decimal not ending with zero", {
+  x <- c(0.12, 1.02, 10.123)
+  expect_equal(drop_trailing_zero(x, align = "left"),
+               c("0.12", "1.02", "10.123"))
+})
 
+test_that("decimal ending with zero", {
+  x <- c(0.120, 1.0200, 10.1000203000, 3.00)
+  expect_equal(drop_trailing_zero(x, align = "left"),
+               c("0.12", "1.02", "10.1000203", "3"))
+})
+
+test_that("integer", {
+  x <- c(1, 10, 010)
+  expect_equal(drop_trailing_zero(x, align = "left"),
+               c("1", "10", "10"))
+})
+
+test_that("NA or NaN", {
+  x <- c(NA, 1, 1.01, NaN, NA_real_,)
+  expect_equal(drop_trailing_zero(x, align = "left"),
+               c("NA", "1", "1.01", "NaN", "NA"))
 })
 
 
-# test_that("input must be numeric numeric", {
-#   x <- 2
-#   expect_equal(drop_trailing_zero(x), "2")
-#
-#   x <- c(1.230, 3.0000, 0.0023400, 10, 100.02)
-#   expect_equal(drop_trailing_zero(x), c("1.23", "3", "0.00234"))
-#
-#   x <- "a"
-#   # expect_error(x)
-#   expect_warning(is.numeric(x), NA)
-#   expect_warning(is.numeric(x), NA)
-#
-# })
+test_that("input is numeric", {
+  x <- "2"
+  expect_error(drop_trailing_zero(x, align = "left"),
+               "Input must be a number or a numeric vector.")
+  x <- c("2", "ab", NA_character_)
+  expect_error(drop_trailing_zero(x, align = "left"),
+               "Input must be a number or a numeric vector.")
+
+})
+
+test_that("aligns", {
+  x <- c(0.120, 1.0200, 10.1000203000, 300.3)
+  expect_equal(drop_trailing_zero(x, align = "left"),
+               c("0.12", "1.02", "10.1000203", "300.3"))
+  expect_equal(drop_trailing_zero(x, align = "right"),
+               c("  0.12", "  1.02", " 10.1000203", "300.3"))
+
+})
